5281b2c66294cef03db5ae26980477d85a1fac89
David Dale <ddale@yandex-team.ru>
2019-06-10 22:08:30 +0300

    fix dependencies

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tgalice",
-    version="0.0.4",
+    version="0.0.6",
     author="David Dale",
     author_email="dale.david@mail.ru",
     description="Yet another common wrapper for Telegram bots and Alice skills",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/avidale/tgalice",
-    packages=["tgalice"],
+    packages=setuptools.find_packages(),
     license="MIT",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -20,5 +20,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['pyTelegramBotAPI', 'textdistance']
+    install_requires=['pyTelegramBotAPI', 'textdistance', 'pyyaml', 'flask']
 )
