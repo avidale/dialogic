8abc851f4d89f1775c031245593c6278abd48db9
David Dale <ddale@yandex-team.ru>
2019-08-01 01:21:24 +0300

    fix importing nlg

--- tgalice/__init__.py
+++ tgalice/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from . import dialog_manager, dialog_connector, session_storage, flask_server, nlu
+from . import dialog_manager, dialog_connector, session_storage, flask_server, nlu, nlg
 from .dialog_manager.base import COMMANDS
 from .message_logging import LoggedMessage
 from tgalice.nlu import basic_nlu
