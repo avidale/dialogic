1d6bdc800c483c0de43cfd7d619ac094a5967d9d
david <dale.david@mail.ru>
2021-03-09 20:12:00 +0300

    fix the pack

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
+import os
 import setuptools
 
-with open("README_en.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+description = "Yet another common wrapper for Alice/Salut skills and Facebook/Telegram/VK bots"
+long_description = description
+if os.path.exists("README_en.md"):
+    with open("README_en.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
 
 setuptools.setup(
     name="dialogic",
-    version="0.3.1",
+    version="0.3.2",
     author="David Dale",
     author_email="dale.david@mail.ru",
-    description="Yet another common wrapper for Alice/Salut skills and Facebook/Telegram/VK bots",
+    description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/avidale/dialogic",
