36985a6a84f8f6cfb2b43475ac63334069c0836d
david <dale.david@mail.ru>
2021-09-27 23:14:27 +0300

    up version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if os.path.exists("README_en.md"):
 
 setuptools.setup(
     name="dialogic",
-    version="0.3.15",
+    version="0.3.16",
     author="David Dale",
     author_email="dale.david@mail.ru",
     description=description,
